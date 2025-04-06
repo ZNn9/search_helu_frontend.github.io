9fc4d60072f21cab0f6a9223d9a4758aaa687ab1
Nguyen Gia Bao <lehonganh2003s@gmail.com>
2025-04-06 11:15:35 +0700

    UPDATE lại đường đẫn
    http://localhost/search_helu_frontend/course/show/{id}/lesson?lesson=i

--- apps/controllers/courseController.php
+++ apps/controllers/courseController.php
@@ -1,15 +1,19 @@
 <?php
-class CourseController {
-    public function index() {
+class CourseController
+{
+    public function index()
+    {
         require_once __DIR__ . '/../views/course/course_list.php';
     }
 
-    public function show($id) {
-
-        if (true) {
-            include __DIR__ . '/../views/course/course_show.php';
+    public function show($id)
+    {
+        if (isset($_GET['lesson'])) {
+            $lessonId = intval($_GET['lesson']); // Lấy ID bài học từ URL
+            // Đường dẫn chính xác đến lesson_detail.php
+            include __DIR__ . '/../views/lesson/lesson_detail.php'; // Hiển thị trang chi tiết bài học
         } else {
-            echo "Không thấy sản phẩm.";
+            include __DIR__ . '/../views/course/course_show.php'; // Hiển thị trang khóa học
         }
     }
 }
